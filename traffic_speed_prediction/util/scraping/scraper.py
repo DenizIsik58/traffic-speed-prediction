6fbc58e19c39a3bb789fa3795301436f458e7523
Deniz Isik <denizisik4600@gmail.com>
2022-03-01 19:58:56 +0100

    refactor consumer.py to scraper.py and add small functions to fetch and create data

---
+++ traffic_speed_prediction/util/scraping/scraper.py
@@ -0,0 +1,33 @@
+import json
+import string
+import requests
+from traffic_speed_prediction.util.config.ReadConfig import read_config, convert_to_date
+
+
+# Fetch data from any given endpoint
+def get_text_from_endpoint(path: string):
+    return requests.get(path).text
+
+
+# Fetch data of a tms station with a specific id
+def get_tms_station_data(id: string):
+    return json.loads(requests.get(read_config()["urls"]["tms_station"]["base_url"] + id).text)
+
+
+def create_db_object(data: json):
+    for item in data['tmsStations'][0]['sensorValues']:
+        if item["sensorUnit"] == "km/h":
+            object_id = item["id"]
+            roadstation_id = item["roadStationId"]
+            speed = item["sensorValue"]
+            date = convert_to_date(item["measuredTime"])[0]
+            time = convert_to_date(item["measuredTime"])[1]
+
+            # Returns a json object
+            # TODO: Make sure to return a list of objects we fetch from the api to insert into the DB
+            return {"object_id": object_id, "roadStationId": roadstation_id, "sensorValue": speed, "date": date,
+                    "time": time}
+
+
+if __name__ == '__main__':
+    print(create_db_object(get_tms_station_data("23001")))
