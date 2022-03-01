cb2f387c321dbc280f2f443e100e6372ce73607b
Deniz Isik <denizisik4600@gmail.com>
2022-03-01 19:59:07 +0100

    Make a function to insert data into the DB

--- traffic_speed_prediction/util/db/DatabaseCommands.py
+++ traffic_speed_prediction/util/db/DatabaseCommands.py
@@ -1,24 +1,27 @@
+import json
 import boto3
-
-
 from traffic_speed_prediction.util.config.ReadConfig import read_secrets
 
-# Connection to AWS
-resource_db = boto3.resource(
-    service_name='dynamodb',
-    aws_access_key_id = read_secrets()["aws_access_key_id"],
-    aws_secret_access_key = read_secrets()["aws_secret_access_key"],
-    region_name = read_secrets()["region_name"]
-)
-
-# Dynamodb table
-table = resource_db.Table('***')
 
 # Put item in table
-table.put_item(
+def insert_to_database(object: json):
+    resource_db = boto3.resource(
+        service_name='dynamodb',
+        aws_access_key_id=read_secrets()["aws_access_key_id"],
+        aws_secret_access_key=read_secrets()["aws_secret_access_key"],
+        region_name=read_secrets()["region_name"]
+    )
+
+    # Dynamodb table
+    table = resource_db.Table(read_secrets()["table_name"])
+
+    table.put_item(
         # Data to be inserted
         Item={
-            '***': '***',
-            '***': '***',
+            'id': object["id"],
+            'roadstation_id' : object["roadStationId"],
+            'average_speed': object["sensorValue"],
+            'date': object["date"],
+            'time': object["time"],
         }
-)
+    )
