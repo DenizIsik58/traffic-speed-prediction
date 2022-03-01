80348c9462b0ae59f85a347977601de31f2754c8
Deniz Isik <deniz_4600@live.dk>
2022-03-01 17:28:43 +0100

    Setup access to secrets variables

--- traffic_speed_prediction/util/db/DatabaseCommands.py
+++ traffic_speed_prediction/util/db/DatabaseCommands.py
@@ -1,14 +1,14 @@
 import boto3
-from dotenv import load_dotenv
+
 
 from traffic_speed_prediction.util.config.ReadConfig import read_secrets
 
 # Connection to AWS
 resource_db = boto3.resource(
     service_name='dynamodb',
-    aws_access_key_id = read_secrets,
-#    aws_secret_access_key = '***',
-#    region_name = '***'
+    aws_access_key_id = read_secrets()["aws_access_key_id"],
+    aws_secret_access_key = read_secrets()["aws_secret_access_key"],
+    region_name = read_secrets()["region_name"]
 )
 
 # Dynamodb table
