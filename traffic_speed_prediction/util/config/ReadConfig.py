27cb3a742aad1ea8b0982fed9584406976148449
Deniz Isik <denizisik4600@gmail.com>
2022-03-03 20:55:41 +0100

    Refactor code - add classes and static methods to make everything more flexible

--- traffic_speed_prediction/util/config/ReadConfig.py
+++ traffic_speed_prediction/util/config/ReadConfig.py
@@ -1,16 +1,19 @@
 import ujson
 
 
-def read_config():
-    with open("../../data.json", "r") as file:
-        return ujson.load(file)
+class Config:
 
+    @staticmethod
+    def read_config():
+        with open("../../data.json", "r") as file:
+            return ujson.load(file)
 
-def read_secrets():
-    with open("../../secrets.json", "r") as file:
-        return ujson.load(file)
+    @staticmethod
+    def read_secrets():
+        with open("../../secrets.json", "r") as file:
+            return ujson.load(file)
 
-
-def convert_to_date(datetime: str):
-    time_registered = str(datetime).split("T")
-    return time_registered[0], time_registered[1].replace("Z", "")
+    @staticmethod
+    def convert_to_date(datetime: str):
+        time_registered = datetime.split("T")
+        return time_registered[0], time_registered[1].replace("Z", "")
--- traffic_speed_prediction/util/db/DatabaseCommands.py
+++ traffic_speed_prediction/util/db/DatabaseCommands.py
@@ -1,29 +1,32 @@
 import json
 import boto3
-from traffic_speed_prediction.util.config.ReadConfig import read_secrets
+from traffic_speed_prediction.util.config.ReadConfig import Config
 
-# TODO: Make a class with commands for the DB
 
-# Put item in table
-def insert_to_database(object: json):
-    # global keyword can be used here to look for variables out of scope
-    resource_db = boto3.resource(
-        service_name='dynamodb',
-        aws_access_key_id=read_secrets()["aws_access_key_id"],
-        aws_secret_access_key=read_secrets()["aws_secret_access_key"],
-        region_name=read_secrets()["region_name"]
-    )
+class DatabaseCommands:
+    # TODO: Make a class with commands for the DB
 
-    # Dynamodb table
-    table = resource_db.Table(read_secrets()["table_name"])
+    # Put item in table
+    @staticmethod
+    def insert_to_database(object: json):
+        # global keyword can be used here to look for variables out of scope
+        resource_db = boto3.resource(
+            service_name='dynamodb',
+            aws_access_key_id=Config.read_secrets()["aws_access_key_id"],
+            aws_secret_access_key=Config.read_secrets()["aws_secret_access_key"],
+            region_name=Config.read_secrets()["region_name"]
+        )
 
-    table.put_item(
-        # Data to be inserted
-        Item={
-            'id': object["id"],
-            'roadstation_id' : object["roadStationId"],
-            'average_speed': object["sensorValue"],
-            'date': object["date"],
-            'time': object["time"],
-        }
-    )
+        # Dynamodb table
+        table = resource_db.Table(Config.read_secrets()["table_name"])
+
+        table.put_item(
+            # Data to be inserted
+            Item={
+                'id': object["id"],
+                'roadstation_id' : object["roadStationId"],
+                'average_speed': object["sensorValue"],
+                'date': object["date"],
+                'time': object["time"],
+            }
+        )
