55d0dbc745ef2c217c6a875f556420ac413fa347
SadlifeCaw <74045978+SadlifeCaw@users.noreply.github.com>
2022-03-07 11:05:05 +0100

    Creating cloudbased solution, Fetching data from API, CI/CD and Docker example (#12)
    * First example of API data extraction
    
    * Add a json file to store variables used in the program to easily access and change values
    
    * Add requirements file for github actions
    
    * Start working on connection to DB
    
    * Setup access to secrets variables
    
    * Add environment variables for your individual secrets for DB connection
    
    * add secrets to gitignore
    
    * Add  table name to secrets
    
    * refactor consumer.py to scraper.py and add small functions to fetch and create data
    
    * Make a function to insert data into the DB
    
    * Add small comment
    
    * Add Ids and names of the tms stations we focus on
    
    * Make a temporary Dataobject class to store values in
    
    * Make an automated fetcher that fetches data every X minutes of the ids given in data.json file
    
    * Update requirements.txt
    
    * Add Docker for the application
    
    * Update docker files
    
    * Add more dependencies to dockerfile
    
    * Delete Idea
    
    * Import and add ujson to increase performance when reading json files
    
    * Remove eval
    
    * Make tostring method more clean
    
    * Add comments and reformat code
    
    * Refactor code - add classes and static methods to make everything more flexible
    
    * Remove dockerignore and add a step more to copy files over
    
    * Remove duplicate installation from dockerfile
    
    * remove secrets.json
    
    * Setup dynamoDB on AWS and refactor insert_to_db method.
    
    * Remove print statement
    
    Co-authored-by: Deniz Isik <deniz_4600@live.dk>
    Co-authored-by: Deniz Isik <denizisik4600@gmail.com>

---
+++ traffic_speed_prediction/util/config/ReadConfig.py
@@ -0,0 +1,19 @@
+import ujson
+
+
+class Config:
+
+    @staticmethod
+    def read_config():
+        with open("../../data.json", "r") as file:
+            return ujson.load(file)
+
+    @staticmethod
+    def read_secrets():
+        with open("../../secrets.json", "r") as file:
+            return ujson.load(file)
+
+    @staticmethod
+    def convert_to_date(datetime: str):
+        time_registered = datetime.split("T")
+        return time_registered[0], time_registered[1].replace("Z", "")
