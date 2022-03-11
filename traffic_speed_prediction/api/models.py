34f32c796b6a75607ccabeba56b14c4cb490129e
Deniz Isik <deniz_4600@live.dk>
2022-03-11 14:22:34 +0100

    Rename fields

--- traffic_speed_prediction/api/models.py
+++ traffic_speed_prediction/api/models.py
@@ -12,20 +12,19 @@ class WeatherHistoryData(models.Model):
 
 class Road(models.Model):
     id = models.IntegerField(primary_key=True)
-    road_sections = ArrayField(ArrayField(models.IntegerField()))
+    roadSections = ArrayField(ArrayField(models.IntegerField()))
 
 
 class Road_section(models.Model):
     id = models.IntegerField(primary_key=True)
     road = models.ForeignKey('Road', on_delete=models.CASCADE)
-    road_temperature = models.TextField()
-    day_light = models.BooleanField()
-    road_condition = models.TextField()
-    weather_symbol = models.TextField()
-    overall_road_condition = models.TextField()
+    roadTemperature = models.TextField()
+    daylight = models.BooleanField()
+    weatherSymbol = models.TextField()
+    overallRoadCondition = models.TextField()
 
 
 class TMS_station(models.Model):
     id = models.IntegerField(primary_key=True)
-    road_number = models.ForeignKey('Road', on_delete=models.CASCADE)
-    road_section = models.ForeignKey('Road_section', on_delete=models.CASCADE)
+    road = models.ForeignKey('Road', on_delete=models.CASCADE)
+    roadSection = models.ForeignKey('Road_section', on_delete=models.CASCADE)
