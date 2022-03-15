abb49fdc7a1a30d2c1a5cbd9b5c115ec6f56075d
jeroleski <74965501+jeroleski@users.noreply.github.com>
2022-03-15 12:02:24 +0100

    More cleaner functionality

--- traffic_speed_prediction/util/data_cleaning/cleaner.py
+++ traffic_speed_prediction/util/data_cleaning/cleaner.py
@@ -1,39 +1,60 @@
-from typing import List, Dict
+from typing import List
 
-EXACT_STRING_LENGTH = 0
-MIN_STRING_LENGTH = 1
-MAX_STRING_LENGTH = 2
-NOT_NULL = 3
-SUB_RULES = 4
+EXACT_STRING_LENGTH = 0  # int
+MIN_STRING_LENGTH = 1  # int
+MAX_STRING_LENGTH = 2  # int
+NOT_NULL = 3  # Any
+SUB_RULES = 4  # Conditions
+EQUALS = 5  # Key
+LESS_THAN = 6  # Key
+MORE_THAN = 7  # Key
+IS_TYPE = 8  # type
+IS_POSITIVE = 9  # Number
+NOT = 10  # Rule
+OR = 11  # (Rule, Rule)
 
 
 class Rule:
-    def __init__(self, type, determinant):
-        self.type = type
+    def __init__(self, rule_type, determinant):
+        self.rule_type = rule_type
         self.determinant = determinant
 
-    def holds(self, val):
-        if self.type == EXACT_STRING_LENGTH:
+    def holds(self, j_obj, val):
+        if self.rule_type == EXACT_STRING_LENGTH:
             return len(val) == self.determinant
-        elif self.type == MIN_STRING_LENGTH:
+        elif self.rule_type == MIN_STRING_LENGTH:
             return len(val) >= self.determinant
-        elif self.type == MAX_STRING_LENGTH:
+        elif self.rule_type == MAX_STRING_LENGTH:
             return len(val) <= self.determinant
-        elif self.type == NOT_NULL:
+        elif self.rule_type == NOT_NULL:
             return val is not None
-        elif self.type == SUB_RULES:
+        elif self.rule_type == SUB_RULES:
             return self.determinant.apply(val)
+        elif self.rule_type == EQUALS:
+            return val == j_obj[self.determinant]
+        elif self.rule_type == LESS_THAN:
+            return val < j_obj[self.determinant]
+        elif self.rule_type == MORE_THAN:
+            return val > j_obj[self.determinant]
+        elif self.rule_type == IS_TYPE:
+            return type(val) == self.determinant
+        elif self.rule_type == IS_POSITIVE:
+            return val >= 0
+        elif self.rule_type == NOT:
+            return not self.determinant.holds(j_obj, val)
+        elif self.rule_type == OR:
+            return self.determinant[0].holds(j_obj, val) or self.determinant[1].holds(j_obj, val)
 
 
 class Conditions:
     def __init__(self, rules):
         self.rules = rules
 
-    def apply(self, jObj):
+    def apply(self, j_obj):
         for key, rules in self.rules.items():
             for rule in rules:
                 try:
-                    if not rule.holds(jObj[key]):
+                    if not rule.holds(j_obj, j_obj[key]):
                         return False
                 except KeyError:
                     return False
@@ -55,7 +76,9 @@ condition1 = Conditions({
     "measuredTime": [Rule(NOT_NULL, None), Rule(EXACT_STRING_LENGTH, 20)],
     "roadAddress": [Rule(SUB_RULES, Conditions({
         "roadSection": [Rule(NOT_NULL, None)]
-    }))]
+    }))],
+    "roadSpeedLimit": [Rule(NOT_NULL, None)],
+    "roadAverageSpeed": [Rule(LESS_THAN, "roadSpeedLimit")]
 })
 
 if __name__ == "__main__":
@@ -79,7 +102,19 @@ if __name__ == "__main__":
         "measuredTime": "2022-03-15T07:49:00Z",
         "roadAddress": {
             "roadSection": 1
-        }
+        },
+        "roadSpeedLimit": 50,
+        "roadAverageSpeed": 48
+    }, {
+        "roadStationId": 4053,
+        "sensorId": 125,
+        "sensorValue": 0.82,
+        "measuredTime": "2022-03-15T07:49:00Z",
+        "roadAddress": {
+            "roadSection": 1
+        },
+        "roadSpeedLimit": 50,
+        "roadAverageSpeed": 62
     }], condition1)
 
     for d in clean_data:
