ab187d483dab116f03c8a8cd41c08d2511b84ec4
Deniz Isik <denizisik4600@gmail.com>
2022-03-16 22:40:04 +0100

    replace arrayfield with manyToMany

--- traffic_speed_prediction/api/models.py
+++ traffic_speed_prediction/api/models.py
@@ -1,12 +1,8 @@
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
+
 # Create your models here.
-
-
-class Road(models.Model):
-    id = models.IntegerField(primary_key=True)
-    roadSections = ArrayField(ArrayField(models.IntegerField()))
 
 
 class Road_section(models.Model):
@@ -16,6 +12,11 @@ class Road_section(models.Model):
     daylight = models.BooleanField()
     weatherSymbol = models.TextField()
     overallRoadCondition = models.TextField()
+
+
+class Road(models.Model):
+    id = models.IntegerField(primary_key=True)
+    road_sections = models.ManyToManyRel(Road_section)
 
 
 class TMS_station(models.Model):
