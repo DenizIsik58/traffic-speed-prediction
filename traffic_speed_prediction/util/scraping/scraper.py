7eb5deaaf5103a2d8245bb4dae01bc8d35745721
Deniz Isik <denizisik4600@gmail.com>
2022-03-16 21:34:45 +0100

    Get a step closer to fixing MODULE_SETTINGS issue

--- traffic_speed_prediction/util/scraping/scraper.py
+++ traffic_speed_prediction/util/scraping/scraper.py
@@ -1,11 +1,11 @@
 import string
 import time
+
 import requests
 import ujson
-from traffic_speed_prediction.api.models import Road
 from traffic_speed_prediction.model.Dataobject import Dataobject
 from traffic_speed_prediction.util.config.ReadConfig import Config
-from traffic_speed_prediction.api.models import Road
+
 
 class Scraper:
     # TODO: Make a scraper class with static methods
@@ -40,17 +40,18 @@ class Scraper:
     def get_road_ids():
 
         # Find all the ids related to road stations and road number
-        for road_condition in ujson.loads(requests.get(Config.read_config()["urls"]["road_sections"]["base_url"]).text)['weatherData']:
+        for road_condition in ujson.loads(requests.get(Config.read_config()["urls"]["road_sections"]["base_url"]).text)[
+            'weatherData']:
             road_number = str(road_condition["id"]).split("_")[0]
             road_sections = []
             # Find all the roadstation ids and road numbers
-            for feature in ujson.loads(requests.get(Config.read_config()["urls"]["road_number"]["base_url"] + road_number).text)["features"]:
-                     road_sections.append(feature["properties"]["roadAddress"]["roadSection"])
+            for feature in \
+            ujson.loads(requests.get(Config.read_config()["urls"]["road_number"]["base_url"] + road_number).text)[
+                "features"]:
+                road_sections.append(feature["properties"]["roadAddress"]["roadSection"])
             print("THESE IDS ARE BOUND TO ROAD NUMBER " + road_number)
             print(road_sections)
             Road(id=road_number, roadSections=road_sections).save()
-
-
 
     @staticmethod
     def repeat_fetching(minutes: int):
@@ -67,4 +68,10 @@ class Scraper:
 
 # Test code
 if __name__ == '__main__':
+
+    from django.conf import settings
+
+    settings.configure()
+
+    from traffic_speed_prediction.api.models import Road
     Scraper.get_road_ids()
