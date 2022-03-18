99eb14b4bd91d38b30eca1342b65b8b2a3fd99c2
Deniz Isik <deniz_4600@live.dk>
2022-03-18 15:25:37 +0100

    Start adding data into database

--- traffic_speed_prediction/util/scraping/scraper.py
+++ traffic_speed_prediction/util/scraping/scraper.py
@@ -3,11 +3,10 @@ import time
 
 import requests
 import ujson
-from traffic_speed_prediction.api.models import Road_section
 
 from util.config.ReadConfig import Config
 
-from api.models import Road
+from api.models import Road, Road_section, TMS_station
 
 
 class Scraper:
@@ -21,29 +20,54 @@ class Scraper:
     def fetch_and_create_db_object_from_tms_station_data():
         pass
 
-
     @staticmethod
     def get_road_ids():
 
         # Find all the ids related to road stations and road number
+        global road_station_id, road_maintenance_class, free_flow_speed1, avg_speed
         for road_condition in ujson.loads(requests.get(Config.read_config()["urls"]["road_sections"]["base_url"]).text)[
             'weatherData']:
+            road_temp = road_condition["roadConditions"][0]["roadTemperature"]
+            daylight = road_condition["roadConditions"][0]["daylight"]
+            weather_symbol = road_condition["roadConditions"][0]["weatherSymbol"]
+
             road_number = str(road_condition["id"]).split("_")[0]
             road_sections = []
+
+            road = Road(Road_number=road_number)
+            road.save()
             # Find all the roadstation ids and road numbers
             for feature in \
-            ujson.loads(requests.get(Config.read_config()["urls"]["road_number"]["base_url"] + road_number).text)[
-                "features"]:
+                    ujson.loads(
+                        requests.get(Config.read_config()["urls"]["road_number"]["base_url"] + road_number).text)[
+                        "features"]:
                 road_sections.append(feature["properties"]["roadAddress"]["roadSection"])
+                road_maintenance_class = feature["properties"]["roadAddress"]["roadMaintenanceClass"]
+                free_flow_speed1 = feature["properties"]["freeFlowSpeed1"]
+                road_station_id = feature["properties"]["roadStationId"]
             print("THESE IDS ARE BOUND TO ROAD NUMBER " + road_number)
+            print(road_number)
             print(road_sections)
-            road = Road(id=road_number, roadSections=road_sections)
-            road.Save()
-            
             for section in road_sections:
-                Road_section(id=section, road=road, roadTemperature=1, daylight=True, weatherSymbol="D200", overallRoadCondition="GOOD").save()
+                if len(road_sections) == 0:
+                    break
+                for station in ujson.loads(requests.get(
+                        Config.read_config()["urls"]["tms_station"]["base_url"] + str(road_station_id)).text)[
+                    "tmsStations"]:
+                    for censor in station["sensorValues"]:
+                        if censor["id"] == str(5122):
+                            avg_speed = station["sensorValue"]
+                            print("AVG SPEED: " + avg_speed)
+                            Road_section(road_section_number=section, road=road, roadTemperature=road_temp,
+                                         daylight=daylight,
+                                         weatherSymbol=weather_symbol, roadMaintenanceClass=road_maintenance_class,
+                                         freeFlowSpeed1=free_flow_speed1,
+                                         average_speed=avg_speed).save()
+                            TMS_station(tms_station=road_station_id, roadSection=section).save()
+                            print("SAVING ROAD TO DB")
+                            break
 
-            print("SAVING ROAD TO DB")
+
 
     @staticmethod
     def repeat_fetching(minutes: int):
@@ -56,6 +80,3 @@ class Scraper:
             for item in Scraper.fetch_and_create_db_object_from_tms_station_data():
                 print(item)
             time.sleep(minutes - ((time.time() - timer) % minutes))
-
-
-
