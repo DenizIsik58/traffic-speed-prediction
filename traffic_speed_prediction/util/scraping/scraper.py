1258b389d2c974a4c3b126c306b5fbe7f4e4f6fc
Deniz Isik <denizisik4600@gmail.com>
2022-03-19 16:33:41 +0100

    Fix redundant data scraping caused by unchanged data

--- traffic_speed_prediction/util/scraping/scraper.py
+++ traffic_speed_prediction/util/scraping/scraper.py
@@ -24,50 +24,66 @@ class Scraper:
     def get_road_ids():
 
         # Find all the ids related to road stations and road number
-        global road_station_id, road_maintenance_class, free_flow_speed1, avg_speed
-        for road_condition in ujson.loads(requests.get(Config.read_config()["urls"]["road_sections"]["base_url"]).text)[
-            'weatherData']:
+
+        for road_condition in ujson.loads(requests.get(Config.read_config()["urls"]["road_sections"]["base_url"]).text)['weatherData']:
             road_temp = road_condition["roadConditions"][0]["roadTemperature"]
             daylight = road_condition["roadConditions"][0]["daylight"]
             weather_symbol = road_condition["roadConditions"][0]["weatherSymbol"]
-
             road_number = str(road_condition["id"]).split("_")[0]
             road_sections = []
+            road_maintenance_classes = []
+            free_flow_speed1s = []
+            road_station_ids = []
 
-            road = Road(Road_number=road_number)
-            road.save()
+            # road = Road(Road_number=road_number)
+            # road.save()
             # Find all the roadstation ids and road numbers
+
             for feature in \
                     ujson.loads(
                         requests.get(Config.read_config()["urls"]["road_number"]["base_url"] + road_number).text)[
                         "features"]:
                 road_sections.append(feature["properties"]["roadAddress"]["roadSection"])
-                road_maintenance_class = feature["properties"]["roadAddress"]["roadMaintenanceClass"]
-                free_flow_speed1 = feature["properties"]["freeFlowSpeed1"]
-                road_station_id = feature["properties"]["roadStationId"]
-            print("THESE IDS ARE BOUND TO ROAD NUMBER " + road_number)
-            print(road_number)
-            print(road_sections)
+                try:
+                    road_maintenance_classes.append(feature["properties"]["roadAddress"]["roadMaintenanceClass"])
+                except:
+                    road_maintenance_classes.append(0)
+                free_flow_speed1s.append(feature["properties"]["freeFlowSpeed1"])
+                road_station_ids.append(feature["properties"]["roadStationId"])
+
+            # print("THESE IDS ARE BOUND TO ROAD NUMBER " + road_number)
+            i = -1
             for section in road_sections:
+
+                i += 1
                 if len(road_sections) == 0:
                     break
                 for station in ujson.loads(requests.get(
-                        Config.read_config()["urls"]["tms_station"]["base_url"] + str(road_station_id)).text)[
+                        Config.read_config()["urls"]["tms_station"]["base_url"] + str(road_station_ids[i])).text)[
                     "tmsStations"]:
                     for censor in station["sensorValues"]:
-                        if censor["id"] == str(5122):
-                            avg_speed = station["sensorValue"]
-                            print("AVG SPEED: " + avg_speed)
-                            Road_section(road_section_number=section, road=road, roadTemperature=road_temp,
-                                         daylight=daylight,
-                                         weatherSymbol=weather_symbol, roadMaintenanceClass=road_maintenance_class,
-                                         freeFlowSpeed1=free_flow_speed1,
-                                         average_speed=avg_speed).save()
-                            TMS_station(tms_station=road_station_id, roadSection=section).save()
+                        if str(censor["id"]) == "5122":
+                            avg_speed = censor["sensorValue"]
+                            print("road_section: " + str(
+                                section) + " road number: " + road_number + " road station number " + str(
+                                road_station_ids[i]))
+                            print("--------------------")
+                            print("AVG SPEED: " + str(avg_speed))
+                            print("weather: " + weather_symbol)
+                            print("daylight: " + str(daylight))
+                            print("roadmain: " + str(road_maintenance_classes[i]))
+                            print("Free flow speed: " + str(free_flow_speed1s[i]))
+                            print("road temp: " + road_temp)
+                            # Road_section(road_section_number=section, road=road, roadTemperature=road_temp,
+                            #            daylight=daylight,
+                            #            weatherSymbol=weather_symbol, roadMaintenanceClass=road_maintenance_class,
+                            #            freeFlowSpeed1=free_flow_speed1,
+                            #            average_speed=avg_speed).save()
+                            # TMS_station(tms_station=road_station_id, roadSection=section).save()
                             print("SAVING ROAD TO DB")
+                            print()
+                            print()
                             break
-
-
 
     @staticmethod
     def repeat_fetching(minutes: int):
