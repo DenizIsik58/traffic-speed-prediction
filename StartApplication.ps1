6f4ffada4a9223eeeaf4c799e2336c946a1308af
Deniz Isik <deniz_4600@live.dk>
2022-03-21 14:15:53 +0100

    Update StartApplication.ps1

--- StartApplication.ps1
+++ StartApplication.ps1
@@ -1,6 +1,6 @@
 #!/bin/sh
 Write-Host "Starting up docker..."
-docker-compose up -d --build
 docker run --name testdb -e POSTGRES_USER=postgres  -e POSTGRES_PASSWORD=1234 -e POSTGRES_DB=testdb -p 5432:5432 -d postgres
+docker-compose up -d --build TSP
 Write-Host "Server is now up and running!"
  #>
