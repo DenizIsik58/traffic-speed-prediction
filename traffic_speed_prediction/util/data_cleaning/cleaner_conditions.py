c6b2b6a870ba66061e6582079d23680e33aeb82d
Adrian Bay Dorph <32704186+adriandorph@users.noreply.github.com>
2022-03-21 14:39:40 +0100

    cleaner_conditions_unittest: Tests for all conditions added

--- traffic_speed_prediction/util/data_cleaning/cleaner_conditions.py
+++ traffic_speed_prediction/util/data_cleaning/cleaner_conditions.py
@@ -3,33 +3,42 @@ from .cleaner import *
 conditions_for_roadNumber = Condition({
     # https://tie.digitraffic.fi/api/v3/metadata/tms-stations/road-number/78
     "features": [
-        Rule(NOT_NULL, None),
+        Rule(NOT_NONE, None),
         Rule(FOR_ALL, Condition({
             "id": [
-                Rule(NOT_NULL, None),
+                Rule(NOT_NONE, None),
                 Rule(IS_POSITIVE, None)
             ],
             "properties": [
-                Rule(NOT_NULL, None),
+                Rule(NOT_NONE, None),
                 Rule(SUB_RULES, Condition({
                     "roadStationId": [
-                        Rule(NOT_NULL, None),
+                        Rule(NOT_NONE, None),
                         Rule(IS_POSITIVE, None)
                     ],
                     "tmsNumber": [
-                        Rule(NOT_NULL, None),
+                        Rule(NOT_NONE, None),
                         Rule(IS_POSITIVE, None),
                     ],
                     "freeFlowSpeed1": [
-                        Rule(NOT_NULL, None),
+                        Rule(NOT_NONE, None),
                         Rule(IS_POSITIVE, None)
                     ],
                     "roadAddress": [
-                        Rule(NOT_NULL, None),
+                        Rule(NOT_NONE, None),
                         Rule(SUB_RULES, Condition({
-                            "roadNumber": [Rule(NOT_NULL, None), None],
-                            "roadSection": [Rule(NOT_NULL, None), None],
-                            "roadMaintenanceClass": [Rule(NOT_NULL, None)]
+                            "roadNumber": [
+                                Rule(NOT_NONE, None),
+                                Rule(IS_POSITIVE, None)
+                            ],
+                            "roadSection": [
+                                Rule(NOT_NONE, None),
+                                Rule(IS_POSITIVE, None)
+                            ],
+                            "roadMaintenanceClass": [
+                                Rule(NOT_NONE, None),
+                                Rule(IS_POSITIVE, None)
+                            ]
                         }))]
                 }))]
         }))]
@@ -37,39 +46,45 @@ conditions_for_roadNumber = Condition({
 
 conditions_for_roadConditions = Condition({
     # https://tie.digitraffic.fi/api/v3/data/road-conditions/54
-    "weatherData": [Rule(FOR_ALL, Condition({
-        "id": [Rule(NOT_NULL, None)],
-        "roadConditions": [Rule(FOR_ALL, Condition({
-            "daylight": [Rule(NOT_NULL, None)],
-            "roadTemperature": [Rule(NOT_NULL, None)],
-            "weatherSymbol": [Rule(NOT_NULL, None)],
-            "overAllRoadCondition": [Rule(NOT_NULL, None)],
+    "weatherData": [
+        Rule(NOT_NONE, None),
+        Rule(FOR_ALL, Condition({
+            "id": [
+                Rule(NOT_NONE, None),
+                Rule(IS_POSITIVE, None)
+            ],
+            "roadConditions": [
+                Rule(NOT_NONE, None),
+                Rule(FOR_ALL, Condition({
+                    "daylight": [Rule(NOT_NONE, None)],
+                    "roadTemperature": [Rule(NOT_NONE, None)],
+                    "weatherSymbol": [Rule(NOT_NONE, None)],
+                    "overAllRoadCondition": [Rule(NOT_NONE, None)],
+                }))]
         }))]
-    }))]
 })
 
 conditions_for_tmsData = Condition({
     # https://tie.digitraffic.fi//api/v1/data/tms-data/23408
     # sensorValue id 5056
     "tmsStations": [
-        Rule(NOT_NULL, None),
+        Rule(NOT_NONE, None),
         Rule(FOR_ALL, Condition({
             "id": [
-                Rule(NOT_NULL, None),
+                Rule(NOT_NONE, None),
                 Rule(IS_POSITIVE, None)
             ],
             "sensorValues": [
-                Rule(NOT_NULL, None),
+                Rule(NOT_NONE, None),
                 Rule(FOR_ALL, Condition({
                     "id": [
-                        Rule(EQUALS, 5122),
-                        Rule(IS_POSITIVE, None)
+                        Rule(EQUALS, 5122)
                     ],
                     "sensorValue": [
-                        Rule(NOT_NULL, None),
+                        Rule(NOT_NONE, None),
                         Rule(IS_POSITIVE, None)
                     ],
-                    "sensorUnit": [Rule(EQUAL, "km/h")]
+                    "sensorUnit": [Rule(EQUALS, "km/h")]
                 }))]
         }))]
 })
