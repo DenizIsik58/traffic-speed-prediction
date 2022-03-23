e0e4f2893f48b7b3c6beb968a6f89447167a98b7
Deniz Isik <deniz_4600@live.dk>
2022-03-23 10:37:27 +0100

    Move application host from local to docker

--- StartApplication.ps1
+++ StartApplication.ps1
@@ -1,6 +1,3 @@
-#!/bin/sh
 Write-Host "Starting up docker..."
-docker run --name testdb -e POSTGRES_USER=postgres  -e POSTGRES_PASSWORD=1234 -e POSTGRES_DB=testdb -p 5432:5432 -d postgres
-docker-compose up -d --build
+docker-compose up -d
 Write-Host "Server is now up and running!"
- #>
--- StartApplication.sh
+++ StartApplication.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+echo "Starting up docker..."
 sudo docker-compose up -d
+echo "Server is now up and running!"
