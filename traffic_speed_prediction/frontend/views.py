d719bc246473d93e3b500ec7e17fe4ae700fe724
carcasonne <villumsonne@tutanota.com>
2022-03-26 16:20:55 +0100

    Add map and fetch coordinates of mouse

--- traffic_speed_prediction/frontend/views.py
+++ traffic_speed_prediction/frontend/views.py
@@ -5,7 +5,7 @@ def index(request, *args, **kwargs):
     return render(request, 'frontend/index.html')
 
 def finmap(request, *args, **kwargs):
-    mapbox_access_token = 'pk.my_mapbox_access_token'
+    mapbox_access_token = 'pk.eyJ1IjoidnNvbi1zb2xpdGEiLCJhIjoiY2wxNmlqcG5jMDdyMjNkcGt1N241bTV3eSJ9'
     return render(request, 'frontend/finmap.html', { 'mapbox_access_token': mapbox_access_token })
 
 def apiSite(request, *args, **kwargs):
