3d1dd60b2381cf8215ff470b4b408a86903902db
Stalin1999 <90043237+Stalin1999@users.noreply.github.com>
2022-04-08 15:19:51 +0200

    Delete sort.py

--- traffic_speed_prediction/traffic_speed_prediction/sort.py
+++
@@ -1,6 +0,0 @@
-import csv
-with open('qgisdata.csv', 'r') as inp, open('databig.csv', 'w') as out:
-    writer = csv.writer(out)
-    for row in csv.reader(inp):
-        if row[0] != "0" :
-            writer.writerow(row)
