578d803364a3855cc36f32939ca4fba55735943b
Deniz Isik <denizisik4600@gmail.com>
2022-04-10 23:05:11 +0200

    Update auto_ml.py

--- traffic_speed_prediction/traffic_speed_prediction/auto_ml.py
+++ traffic_speed_prediction/traffic_speed_prediction/auto_ml.py
@@ -17,11 +17,11 @@ class auto_ml:
     def train():
         global model
 
-        dataset = pd.read_csv('BigData.csv')
+        dataset = pd.read_csv('traffic_speed_prediction/BigData.csv')
         x = dataset.drop(columns=['average_speed'])
         y = dataset['average_speed']
         x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
-        model = autosklearn.regression.AutoSklearnRegressor(time_left_for_this_task=120, per_run_time_limit=30)
+        model = autosklearn.regression.AutoSklearnRegressor(time_left_for_this_task=30, per_run_time_limit=1)
 
         model.fit(x_train, y_train)
 
