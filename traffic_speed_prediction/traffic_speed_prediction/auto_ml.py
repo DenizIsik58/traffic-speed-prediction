5094f01baa1d9119bfbfed41e2f762c47c5cb3f1
Deniz Isik <denizisik4600@gmail.com>
2022-04-10 22:52:30 +0200

    remove print statements and clear manage.py

--- traffic_speed_prediction/traffic_speed_prediction/auto_ml.py
+++ traffic_speed_prediction/traffic_speed_prediction/auto_ml.py
@@ -18,29 +18,14 @@ class auto_ml:
         global model
 
         dataset = pd.read_csv('BigData.csv')
-
-
         x = dataset.drop(columns=['average_speed'])
         y = dataset['average_speed']
         x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
-        model = RandomForestRegressor()
-        #autosklearn.regression.AutoSklearnRegressor(time_left_for_this_task=30, per_run_time_limit=1)
+        model = autosklearn.regression.AutoSklearnRegressor(time_left_for_this_task=120, per_run_time_limit=30)
 
         model.fit(x_train, y_train)
 
         print("DONE")
-        #print(y_train.describe())
-
-        #print("SCORE:")
-        #print("______________________________________________________________ ")
-        #print(model.score(x_test, y_test))
-        y_test_predict = model.predict(x_test)
-        #print(x_test)
-        print(mse(y_test, y_test_predict) ** 0.5)
-        #print("______________________________________________________________ ")
-
-        #y_test_predict = model.predict(x_test)
-        #print(mse(y_test, y_test_predict)**0.5)
         return
 
     @staticmethod
@@ -51,18 +36,3 @@ class auto_ml:
     def getModel():
         return model
 
-    @staticmethod
-    def twoHourTest():
-        dataset10 = pd.read_csv('BigData_10.csv')
-        dataset14 = pd.read_csv('BigData_14.csv')
-        dataset10 = dataset10['average_speed']
-        actual = dataset14['average_speed']
-        dataset14 = dataset14.drop(columns=['average_speed'])
-        predict_14 = model.predict(dataset14)
-
-        print(mse(actual, predict_14)**0.5)
-        print(mse(actual, dataset10)**0.5)
-
-        max = np.argmax(actual - dataset10)
-        print("max : " + str(max))
-        print(actual[max] - dataset10[max])
