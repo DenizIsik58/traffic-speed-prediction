d24c4b20609f0e5c0c6bdc66bcd82ed5f7f74c6e
Deniz Isik <denizisik4600@gmail.com>
2022-04-23 19:22:51 +0200

    Add redirection to other pages

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,32 +1,39 @@
 import React, { Component } from "react";
-import {Route, Router} from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Navbar from "./layout/header";
-import {Switch} from "@mui/material";
-import Map from "./layout/map.js"
+import Map from "./pages/map"
 import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import Faq from "./pages/faq";
 import Footer from './layout/footer'
-
 export default class App extends Component {
     constructor(props) {
         super(props);
     }
 
     render() {
-        return(<div>
+        return(
+                      <Router>
             <div>
-         <Navbar />
-            </div>
                 <div>
-                <Map />
+                    <Navbar />
+                </div>
+                <div>
+                      <Routes>
+                        <Route index element={<Map />} />
+                          <Route path="/finmap" element={<Map />} />
+                        <Route path="/about%20us" element={<About />} />
+                       <Route path="/contact%20us" element={<Contact />} />
+                       <Route path="/faq" element={<Faq />} />
+                      </Routes>
                 </div>
                 <div>
                     <Footer />
                 </div>
 
                 </div>
+                    </Router>
   );
     }
 }
