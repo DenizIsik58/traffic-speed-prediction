94c015570044179ef15262deaf97e75747cdbcf2
Deniz Isik <denizisik4600@gmail.com>
2022-04-27 19:22:04 +0200

    Uncomment frontend from server side for now

--- App/traffic_speed_prediction/traffic_speed_prediction/settings.py
+++ App/traffic_speed_prediction/traffic_speed_prediction/settings.py
@@ -51,8 +51,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders',
-    'frontend.apps.FrontendConfig',
+    'corsheaders'
+    #,'frontend.apps.FrontendConfig',
 ]
 
 MIDDLEWARE = [
